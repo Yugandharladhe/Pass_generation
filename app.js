153fe123045395bf2b0d029cf41a77a6aea8880d
Yugandharladhe <yugandharladhe74@gmail.com>
2023-12-21 15:21:09 +0530

    some change

--- app.js
+++ app.js
@@ -3,16 +3,14 @@ const {generatePass,downloadPass} = require("./generatePass");
 const authenticate = require("./middleware/authenticate");
 var cors = require('cors')
 require("dotenv").config();
-
 const app = express()
-app.use(cors())
 
 const PORT = process.env.PORT || 2000
 app.use(express.json())
 
-app.post("/api/v1/generatePass",authenticate,generatePass);
+app.post("/api/v1/generatePass",cors(),authenticate,generatePass);
 
-app.get("/api/v1/download",authenticate,downloadPass);
+app.get("/api/v1/download",cors(),authenticate,downloadPass);
 
 
 
